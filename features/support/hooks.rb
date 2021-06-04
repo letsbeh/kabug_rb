81a9f0c7dc1784da75f5811b26d46182ac96da90
RICHARD.ARAUJO <richard.araujo@einstein.br>
2021-06-04 17:29:23 -0300

    meu primeiro commit

---
+++ features/support/hooks.rb
@@ -0,0 +1,10 @@
+Before do
+  page.current_window.resize_to(1920, 1080)
+  visit "/"
+end
+
+After do |scenario|
+  screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
+  #embed(screenshot, "image/png", "Screenshot")
+  attach screenshot, "image/png"
+end
